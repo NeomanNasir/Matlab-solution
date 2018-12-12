8cae3110b096d1ea9713995cf032146ed9e8d9bf
Nasir <nnnasir30@gmail.com>
2018-12-12 20:00:51 +0600

    Update area_triangle.m

--- area_triangle.m
+++ area_triangle.m
@@ -1,3 +1,10 @@
-A = [1 7 1; 3 3 1; 5 5 1]
+x1 = input('')
+y1 = input('')
+x2 = input('')
+y2 = input('')
+x3 = input('')
+y3 = input('')
+
+A = [x1 y1 1; x2 y2 1; x3 y3 1]
 ar = det(A);
-area = 0.5*ar
+area = abs(0.5*ar)
